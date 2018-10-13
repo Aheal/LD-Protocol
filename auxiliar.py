87f9d262a258807958bf418242f19801802552ed
Aheal <alejandroalamina@hotmail.com>
2018-10-12 21:49:13 -0500

    Funciones auxiliares

---
+++ auxiliar.py
@@ -0,0 +1,52 @@
+
+def drop(): 
+    print("Get the DUCK out of here!")
+    return False 
+    exit(0) 
+
+## Quita el 0x del cast a hex y agrega 0 de ser necesario
+## esto por el formato del mensaje
+def hexValidate(bytesAmount): 
+    bytesAmount = bytesAmount[2:]
+    bytesAmount = bytesAmount.upper() 
+    if len(bytesAmount) == 1:
+        bytesAmount = "0" + bytesAmount 
+    return bytesAmount
+
+##
+def validate(chain): 
+
+    chainLen = len(chain)
+    bytesAmount = str(hex(int(chainLen / 2))) 
+    bytesAmount = hexValidate(bytesAmount)
+    
+
+    print(f"Chain Length (char): {chainLen}") 
+    print(f"Bytes Amount: {bytesAmount}")
+
+    start = chain[:4]   
+    print(f"Start: {start}")
+    if start != "7878": drop() 
+    
+    end = chain[-4:] 
+    print(f"End: {end}")
+    if end != "0D0A": drop() 
+    
+    temp = chain[4:]
+    pLen = temp[:2] 
+    try: 
+        pLen = int(temp[:2])
+    except: 
+        pass 
+
+    print(f"pLen: {pLen}")
+    if pLen != bytesAmount: drop()
+
+    print("All good Ducky fellas!") 
+    return True
+
+
+chain = "78781Aecjkj232323hjk0D0A78781Aecjkj23hjk0D0A3hjk0D0A"
+
+validate(chain)
+    
