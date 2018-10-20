0d8fc4641e663afd884a6f8e502bc485e55677fe
Aheal <alejandroalamina@hotmail.com>
2018-10-19 20:43:06 -0500

    Restructuracion de clases. Se cambio de abstract factory a factory por termas de rendimiendo. Todas las clases estan en un mismo documento 'LD.py' para hacer funcionar la herencia

--- LD.py
+++ LD.py
@@ -7,8 +7,10 @@
 ## Clase abstracta que recibe el mensaje, lo procesa y
 ## define el protocol number para determina el tipo 
 ## de objeto creado.
-class abtractPackage(object): 
+class abstractPackage(object):
 
+    def __init__(self): 
+        pass
     def __init__(self,chain): 
         self.__chain = chain 
 
@@ -19,12 +21,14 @@ class abtractPackage(object):
 
             * Chain = {self.__chain}
             * pLen = {self.__pLen}
-            * infCont = {self.__infCont}
             * poNum = {self.__poNum}
+            * infCont = {self.__infCont}
             * infSerialNum = {self.__infSerialNum}
             * errChk = {self.__errChk}
         
-        """)
+        """) 
+
+        self.factory()
 
     def mochar(self):
 
@@ -41,8 +45,37 @@ class abtractPackage(object):
 
         temp = self.__chain[-8:]
         infSerialNum = temp[:-4] 
-        return pLen, poNum, infCont, infSerialNum, errChk
-   
+        return pLen, poNum, infCont, infSerialNum, errChk 
+    
+    def factory(self): 
 
-nepe = abtractPackage("78780D01ROMANES00018CDD0D0A") 
+        if(self.__poNum == "01"):
+            login  = Login()
+            return login 
+        if(self.__poNum == "12"):
+            pass
+        if(self.__poNum == "13"):
+            pass
+        if(self.__poNum == "15"):
+            pass 
+        if(self.__poNum == "16"):
+            pass
+        if(self.__poNum == "80"):
+            pass 
+        print("No duck here!") 
+
+    def nepe(self): 
+        print("Hey, Inheritance!")
+   
+class Login(abstractPackage): 
+
+    def __init__(self): 
+        print("i am alive!") 
+        self.nepe()
+
+    def nepe(self): 
+        abstractPackage.nepe(self)
+
+
+nepe = abstractPackage("78780D01ROMANES00018CDD0D0A") 
 
